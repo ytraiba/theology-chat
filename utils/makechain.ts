aa772f3eabf7e93b9b67a534916cceda7d099782
ytraiba <yasintraiba@gmail.com>
2023-04-23 13:36:40 -0400

    Initial commit

---
+++ utils/makechain.ts
@@ -0,0 +1,60 @@
+import { OpenAIChat } from 'langchain/llms';
+import { LLMChain, ChatVectorDBQAChain, loadQAChain } from 'langchain/chains';
+import { PineconeStore } from 'langchain/vectorstores';
+import { PromptTemplate } from 'langchain/prompts';
+import { CallbackManager } from 'langchain/callbacks';
+
+const CONDENSE_PROMPT =
+  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`);
+
+const QA_PROMPT = PromptTemplate.fromTemplate(
+  `You are an AI Imam (a Muslim spiritual leader) that provides life advice based on context.
+  Begin your conversations with Asalam Alaykum. The context is the the muslims's holy-book, the Quran. 
+  You have been asked a related question. Please provide a conversational response.
+  If possible base your response on the context and summarize examples from the Quran.
+
+Question: {question}
+=========
+{context}
+=========
+Answer in Markdown:`,
+);
+
+export const makeChain = (
+  vectorstore: PineconeStore,
+  onTokenStream?: (token: string) => void,
+) => {
+  const questionGenerator = new LLMChain({
+    llm: new OpenAIChat({ temperature: 0 }),
+    prompt: CONDENSE_PROMPT,
+  });
+  const docChain = loadQAChain(
+    new OpenAIChat({
+      temperature: 0,
+      modelName: 'gpt-3.5-turbo', //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
+      streaming: Boolean(onTokenStream),
+      callbackManager: onTokenStream
+        ? CallbackManager.fromHandlers({
+            async handleLLMNewToken(token) {
+              onTokenStream(token);
+              console.log(token);
+            },
+          })
+        : undefined,
+    }),
+    { prompt: QA_PROMPT },
+  );
+
+  return new ChatVectorDBQAChain({
+    vectorstore,
+    combineDocumentsChain: docChain,
+    questionGeneratorChain: questionGenerator,
+    returnSourceDocuments: true,
+    k: 2, //number of source documents to return
+  });
+};
